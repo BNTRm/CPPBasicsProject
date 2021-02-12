44bb62313ea428f3ba8049ace34e6f39498cc718
Viktor <bntrmm@gmail.com>
2021-02-12 18:47:45 +0900

    Homework #5

--- CPPBasicsProject/CPPBasicsProject.cpp
+++ CPPBasicsProject/CPPBasicsProject.cpp
@@ -3,132 +3,223 @@
 
 using namespace std;
 
-int main()
+void printArray(double arr[], unsigned size)
 {
-    // #1
-    cout << "Task #1" << endl;
-
-    int a, b;
-
-    cout << "Enter number A: ";
-    cin >> a;
-    cout << "Enter number B: ";
-    cin >> b;
-
-    cout << "Is number in 10-20 range: ";
-    if (a + b > 9 && a + b < 21)
+    for (size_t i = 0; i < size; i++)
     {
-        cout << "True" << endl;
-    }
-    else
-    {
-        cout << "False" << endl;
+        cout << arr[i] << " ";
     }
 
     cout << endl;
+}
 
-    // #2
-    cout << "Task #2" << endl;
-
-    const int c = 7;
-    const int d = 3;
-
-    if ((c == 10 && d == 10) || c + d == 10)
+void printArray(int arr[], unsigned size)
+{
+    for (size_t i = 0; i < size; i++)
     {
-        cout << "True" << endl;
-    }
-    else
-    {
-        cout << "False" << endl;
+        cout << arr[i] << " ";
     }
 
     cout << endl;
+}
 
-    // #3
-    cout << "Task #3" << endl;
+inline void task1()
+{
+    const unsigned size = 4;
+    double arr[size] = { 3.14, 9.8, 123.456, 42.0 };
 
-    cout << "Odd numbers: ";
-    for (int i = 1; i <= 50; i++)
+    printArray(arr, size);
+
+    cout << endl;
+}
+
+void inverseBinArray(int arr[], unsigned size)
+{
+    for (size_t i = 0; i < size; i++)
     {
-        if (i % 2 == 1)
+        arr[i] = arr[i] == 1 ? 0 : 1;
+    }
+}
+
+inline void task2()
+{
+    const unsigned size = 5;
+    int arr[size] = { 1, 0, 0, 1, 1 };
+
+    printArray(arr, size);
+    inverseBinArray(arr, size);
+    printArray(arr, size);
+
+    cout << endl;
+}
+
+const int TASK_3_ARR_SIZE = 8;
+
+bool fillArray(int* arr)
+{
+    int newArr[] = { 1, 4, 7, 10, 13, 16, 19, 22 };
+
+    for (size_t i = 0; i < TASK_3_ARR_SIZE; i++)
+    {
+        arr[i] = newArr[i];
+    }
+
+    return true;
+}
+
+inline void task3()
+{
+    int arr[TASK_3_ARR_SIZE];
+
+    if (fillArray(arr))
+    {
+        cout << "Array successfully filled" << endl;
+        printArray(arr, TASK_3_ARR_SIZE);
+    }
+
+    cout << endl;
+}
+
+const short TASK_4_ARR_SIZE = 10;
+
+// Алгоритм не работает для значений > TASK_4_ARR_SIZE
+void shiftArray(int* arr, short shift)
+{
+    int shiftedArr[TASK_4_ARR_SIZE];
+
+    for (size_t i = 0; i < TASK_4_ARR_SIZE; i++)
+    {
+        if (shift > 0)
         {
-            cout << i << " ";
-        }
-    }
-
-    cout << endl << endl;
-
-    // #4*
-    cout << "Task #4*" << endl;
-
-    unsigned int prime;
-    cout << "Enter number: ";
-    cin >> prime;
-
-    bool isPrime = true;
-    for (size_t i = 2; i < prime; i++)
-    {
-        if (prime % i == 0)
-        {
-            isPrime = false;
-        }
-    }
-
-    cout << "Is number is Prime: ";
-    if (isPrime)
-    {
-        cout << "True" << endl;
-    }
-    else
-    {
-        cout << "False" << endl;
-    }
-
-    cout << endl;
-
-    // #5*
-    cout << "Task #5*" << endl;
-
-    unsigned short year;
-    while (true) // Цикл для проверки того, что ввели число именно из диапазона 1-3000
-    {
-        cout << "Enter a Year (1-3000): ";
-        cin >> year;
-
-        if (year >= 1 && year <= 3000) {
-            break;
+            if (i < shift)
+            {
+                shiftedArr[i] = arr[TASK_4_ARR_SIZE - (shift - i)];
+            }
+            else
+            {
+                shiftedArr[i] = arr[i - shift];
+            }
         }
         else
         {
-            cout << "Year is not in 1-3000 range" << endl << "Please Re";
-        }
-    }
-
-    bool isLeap = true;
-    if (year % 4 == 0) // Если делится без остатка на 4, то идем дальше
-    {
-        if (year % 100 == 0) // Если делится без остатка на 100, то нужно проверить, не 400 ли это
-        {
-            if (year % 400 != 0)
+            if (i - shift < TASK_4_ARR_SIZE)
             {
-                isLeap = false;
+                shiftedArr[i] = arr[i - shift];
+            }
+            else
+            {
+                shiftedArr[i] = arr[- shift - (TASK_4_ARR_SIZE - i)];
             }
         }
     }
-    else
+
+    for (size_t i = 0; i < TASK_4_ARR_SIZE; i++)
     {
-        isLeap = false;
+        arr[i] = shiftedArr[i];
+    }
+}
+
+inline void task4()
+{
+    int arr[TASK_4_ARR_SIZE];
+
+    for (size_t i = 0; i < TASK_4_ARR_SIZE; i++)
+    {
+        arr[i] = rand() % 100;
     }
 
-    cout << "Year is Leap: ";
-    if (isLeap)
+    cout << "Input array: " << endl;
+    printArray(arr, TASK_4_ARR_SIZE);
+
+    short shift;
+
+    cout << "Enter shift number: ";
+    cin >> shift;
+
+    shiftArray(arr, shift);
+
+    cout << "Shifted array: ";
+    printArray(arr, TASK_4_ARR_SIZE);
+
+    cout << endl;
+}
+
+const short TASK_5_ARR_SIZE = 5;
+
+int leftSum(int* arr, int index)
+{
+    int sum = 0;
+
+    for (size_t i = 0; i <= index; i++)
     {
-        cout << "True" << endl;
+        sum += arr[i];
     }
-    else
+
+    return sum;
+}
+
+int rightSum(int* arr, int index)
+{
+    int sum = 0;
+
+    for (size_t i = index + 1; i < TASK_5_ARR_SIZE; i++)
     {
-        cout << "False" << endl;
+        sum += arr[i];
     }
+
+    return sum;
+}
+
+bool checkBalance(int* arr)
+{
+    for (size_t i = 0; i < TASK_5_ARR_SIZE - 1; i++)
+    {
+        if (leftSum(arr, i) == rightSum(arr, i))
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+inline void task5()
+{
+    int arr1[] = { 1, 1, 1, 2, 1 };
+    int arr2[] = { 2, 1, 1, 2, 1 };
+    int arr3[] = { 10, 1, 2, 3, 4 };
+    int arr4[] = { 4, 5, 6, 10, 5 };
+    int arr5[] = { 20, 30, 40, 50, 60 };
+
+    const string isBalancedText = " Is balanced: ", trueText = "True", falseText = "False";
+
+    printArray(arr1, TASK_5_ARR_SIZE);
+    cout << isBalancedText << (checkBalance(arr1) ? trueText : falseText) << endl;
+
+    printArray(arr2, TASK_5_ARR_SIZE);
+    cout << isBalancedText << (checkBalance(arr2) ? trueText : falseText) << endl;
+
+    printArray(arr3, TASK_5_ARR_SIZE);
+    cout << isBalancedText << (checkBalance(arr3) ? trueText : falseText) << endl;
+
+    printArray(arr4, TASK_5_ARR_SIZE);
+    cout << isBalancedText << (checkBalance(arr4) ? trueText : falseText) << endl;
+
+    printArray(arr5, TASK_5_ARR_SIZE);
+    cout << isBalancedText << (checkBalance(arr5) ? trueText : falseText) << endl;
+}
+
+int main()
+{
+    task1();
+
+    task2();
+
+    task3();
+
+    task4();
+
+    task5();
 
     return 0;
 }
